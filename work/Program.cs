eabbf53a27c6616370e4b0e3a680ce0a1c819e1c
Laniakea <vinograd89@rambler.ru>
2022-01-29 23:49:27 +0300

    write some code

--- work/Program.cs
+++ work/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"Hola", "Ole", "123", "Privet", "Hi", "12345", "void", "arr", "Boo", "Example"};
+
+void PrintArray(string[] arr)
+{
+   for(int i=0; i<arr.Length; i++)
+   {
+        Console.Write($"{arr[i]}  ");
+   }
+}
+
+/*int l = array[3].Length;
+Console.WriteLine(l);*/
+
+string RunArray(string[] finarray)
+{
+    int count = 0;
+    for (int i = 0; i < finarray.Length; i++)
+    {
+        if(finarray[i].Length <= 3) count = count + 1;
+        else count = count + 0;
+    }
+    string[] fin = new string[count];
+    int index = 0;
+     for (int i = 0; i < finarray.Length; i++)
+     {
+        if(finarray[i].Length <= 0) fin[index++] = finarray[i];
+     }
+  return fin[];
+}
+
+
+PrintArray(array);
+Console.WriteLine();
+RunArray(array);
+PrintArray(RunArray(array[]))
